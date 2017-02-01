c589cd598b75abb2d895e90b3bbcf8974d5494a2
Diego Vicente <diegovicente@protonmail.com>
2017-02-01 13:31:47 +0100

    Demo file ready

--- src/run.py
+++ src/run.py
@@ -1,12 +1,48 @@
 from flatland import Flatland
-from agents import GreedyAgent, SupervisedAgent, ReinforcementAgent, EnhancedAgent
+from agents import GreedyAgent, SupervisedAgent, ReinforcementAgent, \
+    EnhancedAgent
 from window import Simulation
 import matplotlib.pyplot as plt
 
 
 def main():
-    # compare_agents(50)
-    run_simulation()
+
+    print("""
+    Demo for IT3708 - Project 1:
+
+    1. Launch the visual simulation with a GreedyAgent
+    2. Train and launch the visual simulation with a SupervisedAgent
+    3. Train and launch the visual simulation with a ReinforcedAgent
+    4. Train and launch the visual simulation with an EnhancedAgent
+
+    5. Launch a visual training with a SupervisedAgent
+    6. Launch a visual training with a ReinforcedAgent
+    7. Launch a visual training with a EnhancedAgent
+
+    8. Run all the agents in text training and plot results
+
+    """)
+
+    choice = input('Enter the option: ')
+
+    if choice == '1':
+        run_simulation('g', False)
+    elif choice == '2':
+        run_simulation('s', True)
+    elif choice == '3':
+        run_simulation('r', True)
+    elif choice == '4':
+        run_simulation('e', True)
+    elif choice == '5':
+        run_simulation('s', False)
+    elif choice == '6':
+        run_simulation('r', False)
+    elif choice == '7':
+        run_simulation('e', False)
+    elif choice == '8':
+        compare_agents(50)
+    else:
+        print("The option entered was not valid.")
 
 
 def compare_agents(rounds):
@@ -46,10 +82,29 @@ def compare_agents(rounds):
     plt.show()
 
 
-def run_simulation():
+def run_simulation(agent, training):
     """Run the graphical simulation after training the agent"""
-    agent = ReinforcementAgent(0.005, 0.99, 1)
-    agent.train(50, False)
+    if agent == 'g':
+        agent = GreedyAgent()
+    elif agent == 's':
+        agent = SupervisedAgent(0.01)
+    elif agent == 'r':
+        agent = ReinforcementAgent(0.005, 0.99, 1)
+    elif agent == 'e':
+        agent = EnhancedAgent(0.005, 0.99, 1)
+
+    if training:
+        env = Flatland(10, 10)
+        agent.train(20, False)
+        agent.new_environment(env)
+        agent.learn(50, False)
+    else:
+        env = Flatland(10, 10)
+        agent.new_environment(env)
+        if isinstance(agent, GreedyAgent):
+            agent.run(50, False)
+        else:
+            agent.learn(50, False)
     simulation = Simulation(agent)
     simulation.start()
 
